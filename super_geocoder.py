73f101afdc77af7a94d2b370b56fc89b323fed04
Alex Stanuta <alex.stanuta@gmail.com>
2021-01-27 10:27:54 +0200

    Change to make it work in Heroku

--- super_geocoder.py
+++ super_geocoder.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, send_file, send_from_directory
 from geopy.geocoders import ArcGIS as ag
+import os
 import pandas as pd
 
 app = Flask(__name__)
@@ -36,4 +37,5 @@ def download():
 		 as_attachment=True)
 
 if __name__ == "__main__":
+	port = int(os.environ.get('PORT', 5000))
 	app.run()
